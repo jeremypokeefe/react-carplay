745d2dc7306b4a562e0cdbfa7283e62782ca5d9d
Jeremy O'Keefe <jeremypokeefe@gmail.com>
2025-03-26 17:27:29 -0700

    WIP

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,23 +1,30 @@
-import { app, shell, BrowserWindow, session, systemPreferences, IpcMainEvent, ipcMain } from 'electron'
-import path, { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import { DEFAULT_CONFIG } from 'node-carplay/node'
-import { Socket } from './Socket'
+import {
+  app,
+  shell,
+  BrowserWindow,
+  session,
+  systemPreferences,
+  IpcMainEvent,
+  ipcMain
+} from 'electron';
+import path, { join } from 'path';
+import { electronApp, optimizer, is } from '@electron-toolkit/utils';
+import { DEFAULT_CONFIG } from 'node-carplay/node';
+import { Socket } from './Socket';
 import * as fs from 'fs';
 import fsExtra from 'fs-extra';
 
-
 // comment below line to allow running on non linux devices
-import {Canbus} from "./Canbus"
+import { Canbus } from './Canbus';
 
-import { ExtraConfig, KeyBindings } from "./Globals";
+import { ExtraConfig, KeyBindings } from './Globals';
 // import CarplayNode, {DEFAULT_CONFIG, CarplayMessage} from "node-carplay/node";
 
-let mainWindow: BrowserWindow
-const appPath: string = app.getPath('userData')
-const configPath: string = appPath + '/config.json'
-console.log(configPath)
-let config: null | ExtraConfig
+let mainWindow: BrowserWindow;
+const appPath: string = app.getPath('userData');
+const configPath: string = appPath + '/config.json';
+console.log(configPath);
+let config: null | ExtraConfig;
 
 const DEFAULT_BINDINGS: KeyBindings = {
   left: 'ArrowLeft',
@@ -30,7 +37,7 @@ const DEFAULT_BINDINGS: KeyBindings = {
   pause: 'KeyO',
   next: 'KeyM',
   prev: 'KeyN'
-}
+};
 
 const EXTRA_CONFIG: ExtraConfig = {
   ...DEFAULT_CONFIG,
@@ -42,56 +49,52 @@ const EXTRA_CONFIG: ExtraConfig = {
   bindings: DEFAULT_BINDINGS,
   most: {},
   canConfig: {}
-}
+};
 
 // comment below line to allow running on non linux devices
-let canbus: null | Canbus
+let canbus: null | Canbus;
 
-let socket: null | Socket
+let socket: null | Socket;
 
 fs.exists(configPath, (exists) => {
-    if(exists) {
-      config = JSON.parse(fs.readFileSync(configPath).toString())
-      let configKeys = JSON.stringify(Object.keys({...config}).sort())
-      let defaultKeys =  JSON.stringify(Object.keys({...EXTRA_CONFIG}).sort())
-      if(configKeys !== defaultKeys) {
-        console.log("config updating")
-        config = {...EXTRA_CONFIG, ...config}
-        console.log("new config", config)
-        fs.writeFileSync(configPath, JSON.stringify(config))
-      }
-      console.log("config read")
-    } else {
-      fs.writeFileSync(configPath, JSON.stringify(EXTRA_CONFIG))
-      config = JSON.parse(fs.readFileSync(configPath).toString())
-      console.log("config created and read")
+  if (exists) {
+    config = JSON.parse(fs.readFileSync(configPath).toString());
+    let configKeys = JSON.stringify(Object.keys({ ...config }).sort());
+    let defaultKeys = JSON.stringify(Object.keys({ ...EXTRA_CONFIG }).sort());
+    if (configKeys !== defaultKeys) {
+      console.log('config updating');
+      config = { ...EXTRA_CONFIG, ...config };
+      console.log('new config', config);
+      fs.writeFileSync(configPath, JSON.stringify(config));
     }
-    socket = new Socket(config!, saveSettings)
-    // comment below if statement to allow running on non linux devices
-    if(config!.canbus) {
-      console.log("Configuring can", config!.canConfig)
-      canbus = new Canbus('can0',  socket, config!.canConfig)
-      canbus.on('lights', (data) => {
-        console.log('lights', data)
-      })
-      canbus.on('reverse', (data) => {
-        mainWindow?.webContents?.send('reverse', data)
-      })
-    }
+    console.log('config read');
+  } else {
+    fs.writeFileSync(configPath, JSON.stringify(EXTRA_CONFIG));
+    config = JSON.parse(fs.readFileSync(configPath).toString());
+    console.log('config created and read');
+  }
+  socket = new Socket(config!, saveSettings);
+  // comment below if statement to allow running on non linux devices
+  if (config!.canbus) {
+    console.log('Configuring can', config!.canConfig);
+    canbus = new Canbus('can0', socket, config!.canConfig);
+    canbus.on('lights', (data) => {
+      console.log('lights', data);
+    });
+    canbus.on('reverse', (data) => {
+      mainWindow?.webContents?.send('reverse', data);
+    });
+  }
+});
 
-})
-
-const handleSettingsReq = (_: IpcMainEvent ) => {
-  console.log("settings request")
-  mainWindow?.webContents.send('settings', config)
-}
-
-
-
+const handleSettingsReq = (_: IpcMainEvent) => {
+  console.log('settings request');
+  mainWindow?.webContents.send('settings', config);
+};
 
 app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
-app.commandLine.appendSwitch('disable-webusb-security', 'true')
-console.log(app.commandLine.hasSwitch('disable-webusb-security'))
+app.commandLine.appendSwitch('disable-webusb-security', 'true');
+console.log(app.commandLine.hasSwitch('disable-webusb-security'));
 function createWindow(): void {
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -108,7 +111,7 @@ function createWindow(): void {
       nodeIntegrationInWorker: true,
       webSecurity: false
     }
-  })
+  });
   app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 
   // mainWindow.webContents.session.setDevicePermissionHandler((details) => {
@@ -122,49 +125,45 @@ function createWindow(): void {
   // })
 
   mainWindow.webContents.session.setPermissionCheckHandler(() => {
-      return true
-  })
+    return true;
+  });
 
   mainWindow.webContents.session.setDevicePermissionHandler((details) => {
-    if(details.device.vendorId === 4884) {
-      return true
+    if (details.device.vendorId === 4884) {
+      return true;
     } else {
-      return false
+      return false;
     }
-
-  })
-
+  });
 
   mainWindow.webContents.session.on('select-usb-device', (event, details, callback) => {
-    event.preventDefault()
+    event.preventDefault();
     const selectedDevice = details.deviceList.find((device) => {
-      return device.vendorId === 4884 && (device.productId === 5408 || device.productId === 5409)
-    })
+      return device.vendorId === 4884 && (device.productId === 5408 || device.productId === 5409);
+    });
 
-    callback(selectedDevice?.deviceId)
-  })
+    callback(selectedDevice?.deviceId);
+  });
   // app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 
   mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+    mainWindow.show();
+  });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
-
-
+    shell.openExternal(details.url);
+    return { action: 'deny' };
+  });
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
   }
   app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
-  systemPreferences.askForMediaAccess("microphone")
+  systemPreferences.askForMediaAccess('microphone');
   mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
     details.responseHeaders!['Cross-Origin-Opener-Policy'] = ['same-origin'];
     details.responseHeaders!['Cross-Origin-Embedder-Policy'] = ['require-corp'];
@@ -176,11 +175,10 @@ function createWindow(): void {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.commandLine.appendSwitch('enable-experimental-web-platform-features');
-app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required")
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 app.whenReady().then(() => {
-
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.electron');
   // const carplay = new CarplayNode(DEFAULT_CONFIG)
   //
   // carplay.start()
@@ -197,45 +195,45 @@ app.whenReady().then(() => {
         'Cross-Origin-Opener-Policy': 'same-origin',
         'Cross-Origin-Embedder-Policy': 'require-corp'
       }
-    })
-  })
+    });
+  });
 
-  ipcMain.on('getSettings', handleSettingsReq)
+  ipcMain.on('getSettings', handleSettingsReq);
 
-  ipcMain.on('saveSettings', saveSettings)
+  ipcMain.on('saveSettings', saveSettings);
 
   ipcMain.on('log-warning', (_, position: string) => {
-  const logLine = `${new Date().toISOString()} - ${position}\n`;
-  const logPath = path.join(app.getPath('userData'), 'warning-log.txt');
-  fs.appendFile(logPath, logLine).catch(console.error);
-});
+    const logLine = `${new Date().toISOString()} - ${position}\n`;
+    const logPath = path.join(app.getPath('userData'), 'warning-log.txt');
+    fs.appendFile(logPath, logLine).catch(console.error);
+  });
 
   // ipcMain.on('startStream', startMostStream)
 
-  ipcMain.on('quit', quit)
+  ipcMain.on('quit', quit);
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+    optimizer.watchWindowShortcuts(window);
+  });
 
-  createWindow()
+  createWindow();
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 const saveSettings = (settings: ExtraConfig) => {
-  console.log("saving settings", settings)
-  fs.writeFileSync(configPath, JSON.stringify(settings))
-  app.relaunch()
-  app.exit()
-}
+  console.log('saving settings', settings);
+  fs.writeFileSync(configPath, JSON.stringify(settings));
+  app.relaunch();
+  app.exit();
+};
 
 // const startMostStream = (_: IpcMainEvent, most: Stream) => {
 //   console.log("stream request")
@@ -245,17 +243,17 @@ const saveSettings = (settings: ExtraConfig) => {
 // }
 
 const quit = (_: IpcMainEvent) => {
-  app.quit()
-}
+  app.quit();
+};
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { connect } from 'mqtt';
-import { SensorPosition } from './types';
+import { SensorPosition } from '../types';
 
-import './App.css'
+import './App.css';
 
-import Settings from "./components/Settings";
+import Settings from './components/Settings';
 import WarningOverlay from './components/WarningOverlay';
-import Info from "./components/Info";
-import Home from "./components/Home";
-import Nav from "./components/Nav";
-import Carplay from './components/Carplay'
-import Camera from './components/Camera'
-import { Box, Modal } from '@mui/material'
-import { useCarplayStore, useStatusStore } from "./store/store";
+import Info from './components/Info';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Carplay from './components/Carplay';
+import Camera from './components/Camera';
+import { Box, Modal } from '@mui/material';
+import { useCarplayStore, useStatusStore } from './store/store';
 
 const MQTT_BROKER = 'ws://192.168.1.100:1883'; // Use ws://<IP>:1883 for remote Pi
 
@@ -26,7 +26,6 @@ const logWarning = (position: SensorPosition) => {
 
 // rm -rf node_modules/.vite; npm run dev
 
-
 const style = {
   position: 'absolute',
   top: '50%',
@@ -35,25 +34,24 @@ const style = {
   height: '95%',
   width: '95%',
   boxShadow: 24,
-  display: "flex"
+  display: 'flex'
 };
 
 function App() {
-  const [receivingVideo, setReceivingVideo] = useState(false)
-  const [commandCounter, setCommandCounter] = useState(0)
-  const [keyCommand, setKeyCommand] = useState('')
-  const [reverse, setReverse] = useStatusStore(state => [state.reverse, state.setReverse])
+  const [receivingVideo, setReceivingVideo] = useState(false);
+  const [commandCounter, setCommandCounter] = useState(0);
+  const [keyCommand, setKeyCommand] = useState('');
+  const [reverse, setReverse] = useStatusStore((state) => [state.reverse, state.setReverse]);
   const [activeWarnings, setActiveWarnings] = useState<SensorPosition[]>([]);
   const [testMode, setTestMode] = useState(true);
 
-  const settings = useCarplayStore((state) => state.settings)
+  const settings = useCarplayStore((state) => state.settings);
 
   const playWarningSound = () => {
-  const audio = new Audio('/sonar.mp3');
-  audio.volume = 0.7;
-  audio.play().catch(console.error);
-};
-
+    const audio = new Audio('/sonar.mp3');
+    audio.volume = 0.7;
+    audio.play().catch(console.error);
+  };
 
   useEffect(() => {
     if (testMode) return;
@@ -91,65 +89,72 @@ function App() {
   }, [testMode]);
 
   useEffect(() => {
-    document.addEventListener('keydown', onKeyDown)
+    document.addEventListener('keydown', onKeyDown);
 
-    return () => document.removeEventListener('keydown', onKeyDown)
+    return () => document.removeEventListener('keydown', onKeyDown);
   }, [settings]);
 
-
   const onKeyDown = (event: KeyboardEvent) => {
-    if(Object.values(settings!.bindings).includes(event.code)) {
-      let action = Object.keys(settings!.bindings).find(key =>
-        settings!.bindings[key] === event.code
-      )
-      console.log(action)
-      if(action !== undefined) {
-        setKeyCommand(action)
-        setCommandCounter(prev => prev +1)
-        if(action === 'selectDown') {
-          console.log('select down')
+    if (Object.values(settings!.bindings).includes(event.code)) {
+      let action = Object.keys(settings!.bindings).find(
+        (key) => settings!.bindings[key] === event.code
+      );
+      console.log(action);
+      if (action !== undefined) {
+        setKeyCommand(action);
+        setCommandCounter((prev) => prev + 1);
+        if (action === 'selectDown') {
+          console.log('select down');
           setTimeout(() => {
-            setKeyCommand('selectUp')
-            setCommandCounter(prev => prev +1)
-          }, 200)
+            setKeyCommand('selectUp');
+            setCommandCounter((prev) => prev + 1);
+          }, 200);
         }
       }
     }
-  }
+  };
 
   return (
     <Router>
-      <div
-        style={{ height: '100%', touchAction: 'none' }}
-        id={'main'}
-        className="App"
-
-      >
+      <div style={{ height: '100%', touchAction: 'none' }} id={'main'} className="App">
         <WarningOverlay activePositions={activeWarnings} testMode={testMode} />
-        <Nav receivingVideo={receivingVideo} settings={settings}/>
-        {settings ? <Carplay  receivingVideo={receivingVideo} setReceivingVideo={setReceivingVideo} settings={settings} command={keyCommand} commandCounter={commandCounter}/> : null}
+        <Nav receivingVideo={receivingVideo} settings={settings} />
+        {settings ? (
+          <Carplay
+            receivingVideo={receivingVideo}
+            setReceivingVideo={setReceivingVideo}
+            settings={settings}
+            command={keyCommand}
+            commandCounter={commandCounter}
+          />
+        ) : null}
         <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/settings"} element={<Settings settings={settings!}/>} />
-          <Route path={"/info"} element={<Info />} />
-          <Route path={"/camera"} element={<Camera settings={settings!}/>} />
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/settings'} element={<Settings settings={settings!} />} />
+          <Route path={'/info'} element={<Info />} />
+          <Route path={'/camera'} element={<Camera settings={settings!} />} />
         </Routes>
-        <Modal
-          open={reverse}
-          onClick={()=> setReverse(false)}
-        >
+        <Modal open={reverse} onClick={() => setReverse(false)}>
           <Box sx={style}>
-            <Camera settings={settings}/>
+            <Camera settings={settings} />
           </Box>
         </Modal>
-        <div style={{
-  position: 'absolute', bottom: 10, left: 10, zIndex: 9999,
-  background: 'rgba(0,0,0,0.6)', color: '#0f0', padding: '10px',
-  fontFamily: 'monospace', fontSize: '12px'
-}}>
-  <div>Active Warnings: {activeWarnings.join(', ') || 'None'}</div>
-  <div>Test Mode: {testMode ? 'ON' : 'OFF'}</div>
-</div>
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 10,
+            left: 10,
+            zIndex: 9999,
+            background: 'rgba(0,0,0,0.6)',
+            color: '#0f0',
+            padding: '10px',
+            fontFamily: 'monospace',
+            fontSize: '12px'
+          }}
+        >
+          <div>Active Warnings: {activeWarnings.join(', ') || 'None'}</div>
+          <div>Test Mode: {testMode ? 'ON' : 'OFF'}</div>
+        </div>
 
         <div style={{ position: 'absolute', top: 10, right: 10, zIndex: 9999 }}>
           <label style={{ color: 'white', fontWeight: 'bold' }}>
@@ -159,8 +164,7 @@ function App() {
         </div>
       </div>
     </Router>
-
-  )
+  );
 }
 
-export default App
+export default App;
